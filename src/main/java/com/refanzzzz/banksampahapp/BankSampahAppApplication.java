53a05cba4cc6c0d38a2da1af70bb1f699cb7d40b
Refanda Surya Saputra <refandasuryasaputra@gmail.com>
2024-11-13 10:49:36 +0700

    initial commit

---
+++ src/main/java/com/refanzzzz/banksampahapp/BankSampahAppApplication.java
@@ -0,0 +1,13 @@
+package com.refanzzzz.banksampahapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSampahAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSampahAppApplication.class, args);
+    }
+
+}
